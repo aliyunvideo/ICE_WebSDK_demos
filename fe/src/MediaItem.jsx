7b37667c7243720511608eb63387c1e43fe0ab60
ewinli <ewinli@163.com>
2022-01-24 15:27:45 +0800

    feat: SearchMediaModal support selecting media type and pagination

--- fe/src/MediaItem.jsx
+++ fe/src/MediaItem.jsx
@@ -2,19 +2,31 @@ import { formatTime } from './utils'
 
 function MediaItem (props) {
   const { item, onClick, selected } = props
+  const mediaType = item.MediaBasicInfo.MediaType
 
-  const url = item.MediaBasicInfo.CoverURL
-  const title = item.FileInfoList[0].FileBasicInfo.FileName
-  const duration = item.FileInfoList[0].FileBasicInfo.Duration
+  let coverUrl
+  let title
+  let duration
+
+  if (mediaType === 'image') {
+    coverUrl = item.FileInfoList[0].FileBasicInfo.FileUrl
+    title = item.FileInfoList[0].FileBasicInfo.FileName
+  } else {
+    coverUrl = item.MediaBasicInfo.CoverURL || 'https://img.alicdn.com/imgextra/i2/O1CN01fRTy3n1ZM1jvBOiyO_!!6000000003179-2-tps-240-136.png'
+    title = item.FileInfoList[0].FileBasicInfo.FileName
+    duration = item.FileInfoList[0].FileBasicInfo.Duration
+  }
 
   const style = {
-    backgroundImage: `url(${url})`
+    backgroundImage: `url(${coverUrl})`
   }
 
   return (
     <div className={`item ${selected ? 'selected' : ''}`} onClick={onClick}>
       <div className='cover' style={style} />
-      <div className='duration'>{formatTime(duration)}</div>
+      {mediaType !== 'image' && (
+        <div className='duration'>{formatTime(duration)}</div>
+      )}
       <div className='title'>{title}</div>
     </div>
   )
