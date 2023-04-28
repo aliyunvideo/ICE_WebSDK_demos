3c567f66e78c6d86242e9185b420da440e4ac34f
liqingwen <qingwen.lqw@alibaba-inc.com>
2023-04-28 16:08:31 +0800

    fix:修复前端依赖文件问题

--- fe/src/index.jsx
+++ fe/src/index.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom'
 import {ConfigProvider} from 'antd'
 import './index.css'
 import App from './App'
 
 ConfigProvider.config({
-  prefixCls: 'tbiz-ant',
+  prefixCls: 'biz-ant',
   iconPrefixCls: 'biz-anticon',
 });
 
 ReactDOM.render(
   <BrowserRouter>
-     <ConfigProvider  prefixCls='biz-ant'   iconPrefixCls= 'biz-anticon' >
-        <App />
-     </ConfigProvider>
+    <ConfigProvider prefixCls='biz-ant' iconPrefixCls='biz-anticon' >
+      <App />
+    </ConfigProvider>
   </BrowserRouter>,
   document.getElementById('root')
 )
